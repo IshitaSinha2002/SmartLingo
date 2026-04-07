a68156762329831ca13526a45674e46c206b2cf3
Ishita Sinha <ishitasinha16@gmail.com>
2026-04-07 20:38:41 +0530

    Adding app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TranslatorUI from "./components/Translator.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <TranslatorUI />
+    </div>
+  );
+}
+
+export default App;
